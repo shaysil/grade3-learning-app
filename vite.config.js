ad03ab7cac3e1f93e48a1fb322037278c9d28bd2
Shay <shay7566@gmail.com>
2025-12-25 00:41:06 +0200

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// חשוב: שם הריפו
 export default defineConfig({
-  base: '/grade3-learning-app/',
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "/grade3-learning-app/",
+});
