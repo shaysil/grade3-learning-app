50922275c54192fc78c9c768b493731100bb93a1
shay silberstein <shay7566@gmail.com>
2025-12-25 12:44:20 +0200

    add the useMemo

--- src/components/modes/EnglishMode.jsx
+++ src/components/modes/EnglishMode.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import QuestionCard from '../question/QuestionCard'
 
 function shuffle(array) {
